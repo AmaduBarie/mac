434b986943ed59a2328fd7543b5de32519f61761
Amadu Barrie <slsales.me@gmail.com>
2020-09-22 04:07:56 +0000

    added

---
+++ build/precache-manifest.df2316ce5ccb745f4f450035bb79e0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a435f03614e4c7c9ecf953bcd7a9a21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6fe1009fbc7dadb25a0",
+    "url": "/static/css/main.5a67881a.chunk.css"
+  },
+  {
+    "revision": "32b9e2affc90d30b0135",
+    "url": "/static/js/2.31c8e3a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.31c8e3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fe1009fbc7dadb25a0",
+    "url": "/static/js/main.2b420861.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
